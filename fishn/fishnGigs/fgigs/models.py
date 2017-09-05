0d921e8b7f4fa2b29754bd6bef3800dbd7e43425
ChadTC <codecrab.me@gmail.com>
2017-09-05 16:54:09 -0700

    just migrated models file for fishery and crew

--- fishn/fishnGigs/fgigs/models.py
+++ fishn/fishnGigs/fgigs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#fisheries and crew in those ports to begin with
+
+class Fishery(models.Model):
+	description_text = models.CharField(max_length=200)
+	opening_date = models.DateTimeField('season start date')
+
+class Crew(models.Model):
+	crew_fishery = models.ForeignKey(Fishery, on_delete=models.CASCADE)
+	crew_text = models.CharField(max_length=200)
+	crew_looks = models.IntegerField(default=0) #how many time someone has looked at their profile
