df4c9b227ce032126a95d66fbc8394ed0109a8e8
ChadTC <codecrab.me@gmail.com>
2017-09-07 12:12:39 -0700

    views for fishery, crew wired in

--- fishn/fishnGigs/fgigs/urls.py
+++ fishn/fishnGigs/fgigs/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# /fgigs/
 	url(r'^$', views.index, name='index'),
+	# /fgigs/5/
+	url(r'^(?P<fishery_id>[0-9]+)/$', views.detail, name='detail'),
+	# /fgigs/5/crew/
+	url(r'^(?P<fishery_id>[0-9]+)/crew/$', views.crew, name='crew'),
+	# /fgigs/5/vote/
+	url(r'^(?P<fishery_id>[0-9]+)/vote/$', views.vote, name='vote'),
 	]
