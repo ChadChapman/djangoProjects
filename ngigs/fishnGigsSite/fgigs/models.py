58be031f9a7b8fddbdb61f4db9d9ffaf66880a13
ChadTC <codecrab.me@gmail.com>
2017-09-19 10:51:06 -0700

    Crew model class filled out, serialized

--- ngigs/fishnGigsSite/fgigs/models.py
+++ ngigs/fishnGigsSite/fgigs/models.py
@@ -59,7 +59,8 @@ class Crew(models.Model):
 	
 	#selected fishery this crew ad is for
 	crew_fishery = models.ForeignKey(Fishery, on_delete=models.CASCADE)
-	
+	#date/time crew available to start work
+	crew_available = models.DateTimeField
 	#blurb crew ad poster can write about themselves
 	crew_text = models.CharField(max_length=200)
 	
@@ -76,16 +77,17 @@ class Crew(models.Model):
 	#years total of fishing experience
 	fishery_exp_years = models.SmallIntegerField()
 	
-	crew_available = models.DateTimeField
-	
+	#the time, date a user created a crew ad to post
 	crew_ad_created = models.DateTimeField(auto_now_add=True)
+	#the amount of time user selected their ad to run, in days eg: 3,5,7
+	crew_ad_runtime = models.IntegerField()
 	
 	#fisheries crew has most experience in
 	fishery_exp_1 = models.CharField(max_length=50)
 	fishery_exp_2 = models.CharField(max_length=50)
 	fishery_exp_3 = models.CharField(max_length=50)
 	#brief summary of any other fishery experience
-	fishery_exp_1 = models.CharField(max_length=150)
+	fishery_exp_other = models.CharField(max_length=150)
 	#whether crew is considered "green" or not
 	#change this to a NullBooleanField() ?
 	is_green = models.BooleanField()
@@ -95,11 +97,31 @@ class Crew(models.Model):
 	#whether crew will to another port for work or not
 	will_travel = models.BooleanField()
 	phone_number = models.CharField(max_length=30)
-	eamil_addr = models.CharField(max_length=50)
+	email_addr = models.CharField(max_length=50)
 	
 	
 	#how many views this crew's ad has gotten
 	#crew_looks = models.IntegerField(default=0) #how many time someone has looked at their profile
 
 	def __str__(self):
-		return self.crew_text
+		#I guess the + is still the fastest/easiest concat method here?
+		info_summary = "Crew info summary: " + crew_first_name + " " +
+		crew_last_name + "/nFishery: " + crew_fishery + "/nAbout: " + crew_text + "/n"
+		"Home port: " + crew_home_port + "Current port, if different: " +
+		crew_current_port + "/nYears experience: " + fishery_exp_years + 
+		"Experience in fisheries:/n" + fishery_exp_1 + " " + fishery_exp_2 +
+		" " + fishery_exp_3 + " " + fishery_exp_other + "/nSkills: " +
+		skills_desc + "/nWill travel: " + will_travel + "/nPhone" + phone_number +
+		" Email: " + email_addr + "/nAvailable to start: " + crew_available
+		return info_summary
+		
+	def crew_is_expired(self):
+		#check to see if the ad should still be displayed or if it has expired
+		#should return false if time difference is positive, true if negative
+		current_time = timezone.now()
+		#eg post on noon monday for 5 days, check at noon tuesday should still
+		#be positive? right? def check this logic again
+		return current_time - datetime.timedelta(days=self.crew_ad_runtime) <=
+		self.crew_ad_created <= now
+		
+	serialized_data = serializers.serialize("json", Crew.objects.all())
