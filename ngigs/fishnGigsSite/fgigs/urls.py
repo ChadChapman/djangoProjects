b6d3cd246c45c6c5bfadaa1d621f75edda800b18
ChadTC <codecrab.me@gmail.com>
2017-09-19 11:20:46 -0700

    working on fgigs/urls routing and views

--- ngigs/fishnGigsSite/fgigs/urls.py
+++ ngigs/fishnGigsSite/fgigs/urls.py
@@ -10,8 +10,23 @@ urlpatterns = [
 	
 	# /fgigs/5/
 	#url(r'^(?P<fishery_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+	#url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 	
+	# /fgigs/fisheries/
+	# show index of all fisheries
+	url(r'^fisheries/$', views.FisheryIndexView.as_view(), name='fisheryindex'),
+			
+	# /fgigs/fisheries/3/
+	# show fishery with pk=3
+	
+	# /fgigs/ports/3/crew/
+	# show crew ads for fishery where fishery pk=3
+	
+	# /fgigs/fisheries/state/2/?
+	# show fisheries in state where state has id or pk=2
+	# need to make State model then? dive into this later
+	
+	####################################################################
 	# /fgigs/5/crew/
 	#url(r'^(?P<fishery_id>[0-9]+)/crew/$', views.crew, name='crew'),
 	url(r'^(?P<fishery_id>[0-9]+)/crew/$', views.crew, name='crew'),
@@ -21,4 +36,3 @@ urlpatterns = [
 	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	]
 
-	################# tut part 3 start here
